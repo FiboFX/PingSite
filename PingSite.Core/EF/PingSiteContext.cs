b2d8e640d2131ad43d6f5d1c46d191e2246dfbb3
Karol Knieć <karolkniecfx@gmail.com>
2018-03-31 19:16:26 +0200

    Updated context

--- PingSite.Core/EF/PingSiteContext.cs
+++ PingSite.Core/EF/PingSiteContext.cs
@@ -8,9 +8,11 @@ namespace PingSite.Core.EF
 {
     public class PingSiteContext : DbContext
     {
-        public DbSet<Host> Hosts { get; set; }
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Room> Rooms { get; set; }
         public DbSet<Category> Categories { get; set; }
-
+        public DbSet<Host> Hosts { get; set; }
+        
         public PingSiteContext(DbContextOptions<PingSiteContext> options) : base(options) { }
     }
 }
