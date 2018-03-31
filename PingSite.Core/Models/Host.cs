76f7ca6c6c8ae25a60a141f5c639d705046668da
Karol Knieć <karolkniecfx@gmail.com>
2018-03-31 17:24:28 +0200

    Added models

---
+++ PingSite.Core/Models/Host.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PingSite.Core.Models
+{
+    public class Host
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public bool LastStatus { get; set; }
+        
+        public Category Category { get; set; }
+    }
+}
