ffff9e02c06b6479b9ae55b8c3d50105199c4a0c
Karol Knieć <karolkniecfx@gmail.com>
2018-04-02 23:51:31 +0200

    Added RoomRepository

---
+++ PingSite.Core/Repositories/IRoomRepository.cs
@@ -0,0 +1,16 @@
+﻿using PingSite.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingSite.Core.Repositories
+{
+    public interface IRoomRepository
+    {
+        Task<IEnumerable<Room>> GetAllAsync();
+        Task AddAsync(Room room);
+        Task UpdateAsync(Room room);
+        Task RemoveAsync(Room room);
+    }
+}
---
+++ PingSite.Core/Repositories/RoomRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PingSite.Core.EF;
+using PingSite.Core.Models;
+
+namespace PingSite.Core.Repositories
+{
+    public class RoomRepository : IRoomRepository
+    {
+        private readonly PingSiteContext _context;
+
+        public RoomRepository(PingSiteContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Room>> GetAllAsync()
+            => await _context.Rooms.ToListAsync();
+
+        public async Task AddAsync(Room room)
+        {
+            await _context.Rooms.AddAsync(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Room room)
+        {
+            _context.Rooms.Update(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(Room room)
+        {
+            _context.Remove(room);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
