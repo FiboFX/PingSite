2d0ebbaf06cb697df696e9ec5c729b28f23ca43a
Karol Knieć <karolkniecfx@gmail.com>
2018-04-03 13:53:29 +0200

    Added HostRepository

---
+++ PingSite.Core/Repositories/HostRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PingSite.Core.EF;
+using PingSite.Core.Models;
+
+namespace PingSite.Core.Repositories
+{
+    public class HostRepository : IHostRepository
+    {
+        private readonly PingSiteContext _context;
+
+        public HostRepository(PingSiteContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Host> GetAsync(int id)
+            => await _context.Hosts.SingleOrDefaultAsync(x => x.Id == id);
+
+        public async Task<IEnumerable<Host>> GetAllAsync()
+            => await _context.Hosts.ToListAsync();
+
+        public async Task AddAsync(Host host)
+        {
+            await _context.Hosts.AddAsync(host);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Host host)
+        {
+            _context.Hosts.Update(host);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(Host host)
+        {
+            _context.Hosts.Remove(host);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ PingSite.Core/Repositories/IHostRepository.cs
@@ -0,0 +1,17 @@
+﻿using PingSite.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingSite.Core.Repositories
+{
+    public interface IHostRepository
+    {
+        Task<Host> GetAsync(int id);
+        Task<IEnumerable<Host>> GetAllAsync();
+        Task AddAsync(Host host);
+        Task UpdateAsync(Host host);
+        Task RemoveAsync(Host host);
+    }
+}
