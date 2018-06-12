f50573c7ffa82269f9890b62869de0d3c7560fde
Karol Knieć <karolkniecfx@gmail.com>
2018-06-12 15:09:01 +0200

    Added CategoryId for HostDto

--- PingSite.Core/DTO/HostDto.cs
+++ PingSite.Core/DTO/HostDto.cs
@@ -10,5 +10,6 @@ namespace PingSite.Core.DTO
         public string Name { get; set; }
         public string Address { get; set; }
         public bool LastStatus { get; set; }
+        public int? CategoryId { get; set; }
     }
 }
