a4b39b3f722784bca080de3939d6c1b570e7847a
Karol Knieć <karolkniecfx@gmail.com>
2018-07-01 14:59:10 +0200

    Added more default categories

--- scripts/database.sql
+++ scripts/database.sql
@@ -19,6 +19,14 @@ CREATE TABLE Categories (
 	ImgUrl NVARCHAR(250) NOT NULL
 )
 
+INSERT INTO Categories(Name, ImgUrl) VALUES('PC', '/images/computer.png')
+INSERT INTO Categories(Name, ImgUrl) VALUES('Website', '/images/website.png')
+INSERT INTO Categories(Name, ImgUrl) VALUES('Smartphone', '/images/smartphone.png')
+INSERT INTO Categories(Name, ImgUrl) VALUES('Game console', '/images/game-console.png')
+INSERT INTO Categories(Name, ImgUrl) VALUES('Router', '/images/router.png')
+INSERT INTO Categories(Name, ImgUrl) VALUES('Notebook', '/images/notebook.png')
+INSERT INTO Categories(Name, ImgUrl) VALUES('Camera', '/images/camera.png')
+
 CREATE TABLE Hosts (
 	Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Name NVARCHAR(50) NOT NULL,
